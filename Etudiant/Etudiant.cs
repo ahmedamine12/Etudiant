9147e921b8f64044d737ee12ca30067d1ee13eb3
ahmedamine12 <ahmedamine.bouricha@etu.fstm.ac.ma>
2022-02-14 15:08:22 +0100

    Add project files.

---
+++ Etudiant/Etudiant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etudiant
+{
+    public class Etudiant
+    {
+        public string nom { get; set; }
+        public string  prenom { get; set; }
+        public int ID { get; set; }
+        public Etudiant(){ }
+        public Etudiant(string nom ,string pre,int id)
+        {
+            this.nom = nom;
+            this.prenom = pre;
+            this.ID = id;
+        }
+    }
+}
---
+++ Etudiant/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Json.Net;
+
+
+namespace Etudiant
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Etudiant> lst = new List<Etudiant>();
+            lst.Add(new Etudiant( "Amine", "Bouricha",1));
+            lst.Add(new Etudiant( "Ahmed", "Bouricha",2));
+            lst.Add(new Etudiant( "houda", "Ahbela",3));
+            lst.Add(new Etudiant( "Zineb", "Abila",4));
+
+
+           string chaine = JsonNet.Serialize(lst);
+           
+            
+            Console.WriteLine(chaine);
+            File.WriteAllText("Info.json", chaine);
+            Console.ReadKey();
+        }
+    }
+}
